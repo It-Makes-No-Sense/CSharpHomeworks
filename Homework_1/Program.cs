a196760092faffe829d9eb22ee520119cef7ce46
Grisha-NoSense <Devimp15@mail.ru>
2023-06-11 16:57:34 +0300

    Rename exercises 1,2 into 2,4. Add exercise 6

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,5 +1,5 @@
-﻿// Задание №1
-// Console.WriteLine("Задание 1");
+﻿// Задание №2
+// Console.WriteLine("Задание 2");
 
 
 // Console.WriteLine("Введите Первое Число");
@@ -24,61 +24,81 @@
 // }
 // Console.Write($"{max} большее число,а {min} меньшее");
 
-// Задание 2
-Console.WriteLine("Задание 2");
+// Задание №4
+// Console.WriteLine("Задание 4");
 
-Console.WriteLine("Введите Первое Число");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Первое Число");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите Второе Число");
-int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Второе Число");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите Третье Число");
-int z = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите Третье Число");
+// int z = Convert.ToInt32(Console.ReadLine());
 
-int min;
-int max;
+// int min;
+// int max;
 
-if (y>x)
-{
-      if (x>z)
-      {
-            max = y;
-            min = z;
-      }
-      else
-      {
-            min = x;
-            if (y>z)
-            {
-                  max = y;
-            }
-            else
-            {
-                  max = z;
-            }
-      }
-}
-else
-{
-      if (y>z)
-      {
-            max = x;
-            min = z;
-      }
-      else
-      {
-            min = y;
-            if (x>z)
-            {
-                  max = x;
-            }
-            else
-            {
-                  max = z;
-            }
-      }
-}
+// if (y>x)
+// {
+//       if (x>z)
+//       {
+//             max = y;
+//             min = z;
+//       }
+//       else
+//       {
+//             min = x;
+//             if (y>z)
+//             {
+//                   max = y;
+//             }
+//             else
+//             {
+//                   max = z;
+//             }
+//       }
+// }
+// else
+// {
+//       if (y>z)
+//       {
+//             max = x;
+//             min = z;
+//       }
+//       else
+//       {
+//             min = y;
+//             if (x>z)
+//             {
+//                   max = x;
+//             }
+//             else
+//             {
+//                   max = z;
+//             }
+//       }
+// }
 
+// Console.Write($"{max} большее число,а {min} меньшее");
 
-Console.Write($"{max} большее число,а {min} меньшее");
+// Задание №6
+// Console.WriteLine("Задание 6");
+
+// Console.WriteLine("Введите число");
+// int X = Convert.ToInt32(Console.ReadLine());
+
+// if (X % 2 == 0)
+// {
+//       Console.WriteLine($"Число {X} четное");
+// }
+// else
+// {
+//       Console.WriteLine($"Число {X} нечетное");
+// }
+
+// Задание №8
+Console.WriteLine("Задание 8");
+
+Console.WriteLine("Введите число");
+int X = Convert.ToInt32(Console.ReadLine());
