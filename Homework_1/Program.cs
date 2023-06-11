cfca624aa2d57ab2cb031e3f756280e58ca586d0
Grisha-NoSense <Devimp15@mail.ru>
2023-06-11 16:49:48 +0300

    Add exercises 1 and 2

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -1,1 +1,84 @@
-﻿
+﻿// Задание №1
+// Console.WriteLine("Задание 1");
+
+
+// Console.WriteLine("Введите Первое Число");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите Второе Число");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// int min;
+// int max;
+
+// if (y>x)
+// {
+//       max = y;
+//       min = x;
+// }
+
+// else
+// {
+//       max = x;
+//       min = y;
+// }
+// Console.Write($"{max} большее число,а {min} меньшее");
+
+// Задание 2
+Console.WriteLine("Задание 2");
+
+Console.WriteLine("Введите Первое Число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Второе Число");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите Третье Число");
+int z = Convert.ToInt32(Console.ReadLine());
+
+int min;
+int max;
+
+if (y>x)
+{
+      if (x>z)
+      {
+            max = y;
+            min = z;
+      }
+      else
+      {
+            min = x;
+            if (y>z)
+            {
+                  max = y;
+            }
+            else
+            {
+                  max = z;
+            }
+      }
+}
+else
+{
+      if (y>z)
+      {
+            max = x;
+            min = z;
+      }
+      else
+      {
+            min = y;
+            if (x>z)
+            {
+                  max = x;
+            }
+            else
+            {
+                  max = z;
+            }
+      }
+}
+
+
+Console.Write($"{max} большее число,а {min} меньшее");
