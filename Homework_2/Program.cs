0560e1c486f3ab97bb547755bc3c8239d06d174d
Grisha-NoSense <Devimp15@mail.ru>
2023-06-17 17:44:40 +0300

    Add exercise 23

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -30,27 +30,53 @@
 // }
 
 
+
+
 // Задача 21
 // Напишите программу,которая принимает на вход координаты двух точек 
 // и находит расстояние между ними в 3D пространстве.
 
-Console.WriteLine("Введите координату X для точки A");
-int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату X для точки A");
+// int x1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Y для точки A");
-int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y для точки A");
+// int y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Z для точки A");
-int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Z для точки A");
+// int z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату X для точки B");
-int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату X для точки B");
+// int x2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Y для точки B");
-int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Y для точки B");
+// int y2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координату Z для точки B");
-int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату Z для точки B");
+// int z2 = Convert.ToInt32(Console.ReadLine());
 
-double res = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
-Console.WriteLine($"Расстояние между AB в 3D пространстве = {res}");
+// double res = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+// Console.WriteLine($"Расстояние между AB в 3D пространстве = {res}");
+
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int qube;
+
+// Console.Write($"{num} -> ");
+// for (int i= 1; i<=num;i++)
+// {
+//       qube = Convert.ToInt32(Math.Pow(i,3));
+//       if (i<num)
+//       {
+//             Console.Write($"{qube}, ");
+//       }
+//       else
+//       {
+//             Console.Write($"{qube}");
+//       }
+// }
