7f266ce190869d75dcf9a285d2d70f3c153c0933
Grisha-NoSense <Devimp15@mail.ru>
2023-06-24 22:53:37 +0300

    add Homework 4 and add exercise 34

---
+++ Homework_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный 
+// случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// int[] getArray(int size)
+// {
+//       int[] array = new int[size];
+//       for (int i = 0; i < array.Length; i++)
+//       {
+//             array[i] = new Random().Next(100,999);
+//       }
+//       return array;
+// }
+
+// int[] Array = getArray(5);
+// Console.Write($"{String.Join(',',Array)} -->");
+// int count = 0;
+// for (int i = 0; i < Array.Length; i++)
+// {
+//       if (Array[i] % 2 == 0)
+//       {
+//             count++;
+//       }
+// }
+// Console.Write($"{count}");
