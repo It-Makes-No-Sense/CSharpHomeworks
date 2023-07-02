fe6ea0179868c198a67785badedef9a3e9cb39a6
Grisha-NoSense <Devimp15@mail.ru>
2023-07-02 13:28:53 +0300

    Add exercise 52

--- Homework_5/Program.cs
+++ Homework_5/Program.cs
@@ -95,3 +95,59 @@
 // PrintArray(array);
 // checkIndex(array);
 
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое* элементов в каждом столбце.
+
+// int[,] getArray()
+// {
+//       int rows = new Random().Next(2,5);
+
+//       int columns = new Random().Next(2,5);
+
+//       Console.WriteLine($"m = {rows}, n = {columns}");
+
+//       int[,] array = new int[rows,columns];
+
+//       for (int i = 0; i < rows; i++)
+//       {
+//             for (int j = 0; j < columns; j++)
+//             {
+//                   array[i,j] = new Random().Next(1,10);
+//             }
+//       }
+//       return array;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//       for (int i = 0; i < array.GetLength(0); i++)
+//       {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                   Console.Write($"{array[i,j]} ");
+//             }
+//             Console.WriteLine();
+//       }
+// }
+
+// void findMiddle(int[,] array)
+// {
+//       double sum;
+//       double mid;
+//       for (int i = 0; i < array.GetLength(1); i++)
+//       {
+//             sum = 0;
+//             for (int j = 0; j < array.GetLength(0); j++)
+//             {
+//                   sum += array[j,i];
+//             }
+//             mid =Math.Round(sum/array.GetLength(0),1);
+//             Console.Write($"{mid}; ");
+//       }
+// }
+
+// int[,] array = getArray();
+// PrintArray(array);
+// findMiddle(array);
