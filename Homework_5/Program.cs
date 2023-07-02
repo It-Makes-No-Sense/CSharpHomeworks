9703bbafce199c9054af1cd6b11a9a5bccf26ad3
Grisha-NoSense <Devimp15@mail.ru>
2023-07-02 12:49:02 +0300

    Create Homework_5 and add exercise 47

---
+++ Homework_5/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными(тип double) числами.
+
+// double[,] getArray()
+// {
+//       Console.WriteLine("Введите кол-во строк массива");
+//       int rows = int.Parse(Console.ReadLine()!);
+
+//       Console.WriteLine("Введите кол-во столбцов массива");
+//       int columns = int.Parse(Console.ReadLine()!);
+
+//       double[,] array = new double[rows,columns];
+
+//       for (int i = 0; i < rows; i++)
+//       {
+//             for (int j = 0; j < columns; j++)
+//             {
+//                   array[i,j] = Math.Round(new Random().NextDouble(),2);
+//             }
+//       }
+//       return array;
+// }
+
+// void PrintArray(double[,] array)
+// {
+//       for (int i = 0; i < array.GetLength(0); i++)
+//       {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                   Console.Write($"{array[i,j]} ");
+//             }
+//             Console.WriteLine();
+//       }
+// }
+
+// double[,] array = getArray();
+// PrintArray(array);
